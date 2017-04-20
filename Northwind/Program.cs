6b97cfcf27e8d62130af3519f507d20709c6bc87
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-04-20 14:55:05 +0100

    Making the document store instance singleton

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -11,21 +11,29 @@ namespace Northwind
     class Program
     {
         static void Main(string[] args)
-        {
-            var documentStore = new DocumentStore
-            {
-                Url = "http://localhost:8080",
-                DefaultDatabase = "Northwind"
-            };
-
-            documentStore.Initialize();
-
-            using (var session = documentStore.OpenSession())
+        {           
+            using (var session = DocumentStoreHolder.Store.OpenSession())
             {
                 var p = session.Load<dynamic>("products/2");
                 System.Console.WriteLine(p.Name);
                 Console.ReadLine();
             }
         }
+    }
+
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore = new Lazy<IDocumentStore>(() =>
+        {
+            var store = new DocumentStore
+            {
+                Url = "http://localhost:8080",
+                DefaultDatabase = "Northwind"
+            };
+
+            return store.Initialize();
+        });
+
+        public static IDocumentStore Store => LazyStore.Value;
     }
 }
