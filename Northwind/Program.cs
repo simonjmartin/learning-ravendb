16e01c55639ef9f428f113887e8940a3dcbf2fad
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-04-21 11:41:02 +0100

    Classes stubbed up
    Include approach allows multiple tables to be queried without needing multiple db triips.
    
    New interface which requests and order number

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -12,14 +12,62 @@ namespace Northwind
     {
         static void Main(string[] args)
         {
-            GetMultipleDocuments();
+            RequestAnOrderNumber();
+        }
+
+        private static void RequestAnOrderNumber()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please, enter an order # (0 to exit):");
+
+                int orderNumber;
+                if(!int.TryParse(Console.ReadLine(), out orderNumber))
+                {
+                    Console.WriteLine("Order # is invalid!");
+                    continue;
+                }
+
+                if (orderNumber == 0) break;
+
+                PrintOrder(orderNumber);
+            }
+            
+        }
+        private static void PrintOrder(int orderNumber)
+        {
+            
+        }
+
+        private static void LoadRelatedData()
+        {
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var order = session
+                    .Include<Order>(x => x.Company)
+                    .Include(x => x.Employee)
+                    .Include(x => x.Lines.Select(l => l.Product))
+                    .Load("orders/1");
+
+            }
+        }
+
+        private static void LoadMultipleTypes()
+        {
+            //using (var session = DocumentStoreHolder.Store.OpenSession())
+            //{
+            //    object[] items = session.Load<object>("products/1", "categories/2");
+
+            //    Product product = (Product)items[0];
+            //    Category category = (Category)items[1];
+            //}
         }
 
         private static void GetMultipleDocuments()
         {
             using (var session = DocumentStoreHolder.Store.OpenSession())
             {
-                Product[] p = session.Load<Product>(1,2,3);
+                Product[] p = session.Load<Product>(1, 2, 3);
 
                 foreach (var item in p)
                 {
@@ -48,11 +96,6 @@ namespace Northwind
                 Console.ReadLine();
             }
         }
-    }
-
-    public class Product
-    {
-        public string Name { get; set; }
     }
 
     public static class DocumentStoreHolder
