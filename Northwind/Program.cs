8fddf8a3c42d75f4145ebfd92d11e561b956351e
Simon Martin (simon.martin@canterbury.ac.uk) <simon.martin@canterbury.ac.uk>
2017-04-21 11:47:18 +0100

    Print order, company, employee and line details

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -36,7 +36,35 @@ namespace Northwind
         }
         private static void PrintOrder(int orderNumber)
         {
-            
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var order = session
+                    .Include<Order>(o => o.Company)
+                    .Include(o => o.Employee)
+                    .Include(o => o.Lines.Select(l => l.Product))
+                    .Load(orderNumber);
+
+                if (order == null)
+                {
+                    Console.WriteLine($"Order # {orderNumber} not found.");
+                    return;
+                }
+
+                Console.WriteLine($"Order #{orderNumber}");
+
+                var c = session.Load<Company>(order.Company);
+                Console.WriteLine($"Company: {c.Id} - {c.Name}");
+
+                var e = session.Load<Employee>(order.Employee);
+                Console.WriteLine($"Employee: {e.Id} - {e.LastName}, {e.FirstName}");
+
+                foreach (var orderLine in order.Lines)
+                {
+                    var p = session.Load<Product>(orderLine.Product);
+                    Console.WriteLine($"  - {orderLine.ProductName}, " +
+                        $" {orderLine.Quantity} x {p.QuantityPerUnit}");
+                }
+            }
         }
 
         private static void LoadRelatedData()
